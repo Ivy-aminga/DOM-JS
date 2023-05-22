5b4daff00a9db206e293b9c7ffd2f2b0cb1ab369
Ivy-aminga <amingaivy@gmail.com>
2023-05-22 19:57:26 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,24 @@
+
+document.body.style.backgroundColor = 'silver'
+
+document.getElementById('title').style.color='green'
+document.getElementById('title').style.fontSize = '40px'
+document.getElementById('title').style.textAlignLast='center'
+
+document.getElementById('para').style.fontSize='24px'
+document.getElementById('para').style.color='brown'
+
+document.getElementById('fru').style.textTransform='Uppercase'
+document.getElementById('veg').style.textTransform='Uppercase'
+
+let newFruit = document.createElement("li");
+newFruit.innerHtml= "Grapes"
+document.getElementById("fruList").appendChild("newFruit");
+
+let newVeg = document.createElement("li");
+newVeg.innerHtml= "Spinach"
+document.getElementById("vegList").appendChild("newVeg");
+
+
+
+
